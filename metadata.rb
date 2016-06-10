a6908cc6b8a0748b0e6c593b9c15ad2a153bbb8b
Isaac Gittins <isaacgittins@localhost.localdomain>
2016-06-10 15:06:11 +1000

    Version Bump: 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license 'MIT'
 description 'Installs Gradle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 recipe 'gradle::tarball', 'installs gradle from tarball'
-version '1.0.0'
+version '1.0.1'
 
 depends 'ark', '>= 0.0.0'
