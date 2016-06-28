20943e2ced911733a9d3baea9f06118c85e5d92e
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-06-28 14:38:19 -0400

    Add supports details to metadata

--- metadata.rb
+++ metadata.rb
@@ -8,5 +8,8 @@ version          '1.0.0'
 
 recipe           'gradle::tarball', 'installs gradle from tarball'
 
+supports 'ubuntu', '= 14.04'
+supports 'centos', '= 7.2'
+
 depends 'java', '~> 1.39'
 depends 'ark', '~> 1.0'
