9cab3b260b11abe20eab41325a96ba6bb556d8f8
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-06-28 14:20:54 -0400

    Bump to v1.0.0, new cookbook name initial release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'MIT'
 description      'Installs Gradle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.0'
 
 recipe           'gradle::tarball', 'installs gradle from tarball'
 
