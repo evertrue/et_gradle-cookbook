7415df2c22f04982346768102f0963650a01261e
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-06-28 14:38:27 -0400

    Add issues & source URLs to metadata

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,9 @@ version          '1.0.0'
 
 recipe           'gradle::tarball', 'installs gradle from tarball'
 
+issues_url 'https://github.com/evertrue/et_gradle-cookbook/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/evertrue/et_gradle-cookbook' if respond_to?(:source_url)
+
 supports 'ubuntu', '= 14.04'
 supports 'centos', '= 7.2'
 
