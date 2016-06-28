2f55cc3933c3384d3299531f0224b9006dde744f
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-06-28 14:39:21 -0400

    Remove unnecessary recipe bit from metadata

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,6 @@ description      'Installs Gradle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
-recipe           'gradle::tarball', 'installs gradle from tarball'
-
 issues_url 'https://github.com/evertrue/et_gradle-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/evertrue/et_gradle-cookbook' if respond_to?(:source_url)
 
