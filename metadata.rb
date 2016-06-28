dd01bec40fab894c1e534ee907cc1f933e90f499
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-06-28 14:38:36 -0400

    Bump to v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'MIT'
 description      'Installs Gradle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 recipe           'gradle::tarball', 'installs gradle from tarball'
 
